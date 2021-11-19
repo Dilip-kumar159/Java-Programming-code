80efcd84616c8058baa416ad0ede16426fef2a7e
Dilip <dilip45377@gmail.com>
2021-11-19 17:48:31 +0530

    Programs executed

---
+++ src/com/company/Pattern_02.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Pattern_02 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i = 1;
+        while (i<=n){
+            int j = 1;
+            while(j<=n){
+                System.out.print(i + " ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
---
+++ src/com/company/TypeConversionBabbar.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class TypeConversionBabbar {
+    public static void main(String[] args) {
+        int a = 'a';
+        System.out.println(a);
+
+        char c = 98;
+        System.out.println(c);
+
+        char ch = 12345;
+        System.out.println(ch);
+    }
+}
